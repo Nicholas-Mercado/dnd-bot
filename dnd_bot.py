0166385e5e75eaa3fa592e0d0313839241ec7c6c
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-09-15 12:21:17 -0700

    can request

--- dnd_bot.py
+++ dnd_bot.py
@@ -1,4 +1,17 @@
-
+import requests
     
 class Dnd_Bot:
-    pass  
+    
+    def __init__(self, fact=None):
+        self.fact=fact
+        
+    def fact_request(self):
+        r = requests.get("https://www.dnd5eapi.co/api/ability-scores/str")
+        self.fact = r.json
+        
+    
+
+if __name__ == '__main__':
+    fact1 =Dnd_Bot()
+    fact1.fact_request()
+    print(fact1)
--- test/dnd_bot_test.py
+++ test/dnd_bot_test.py
@@ -3,3 +3,12 @@ from dnd_bot import Dnd_Bot
 
 def test_exists():
     assert Dnd_Bot
+
+def test_instantiate():
+    assert Dnd_Bot()
+
+def test_requests():
+    fact1 =Dnd_Bot()
+    fact1.fact_request()
+    assert fact1.fact
+    
