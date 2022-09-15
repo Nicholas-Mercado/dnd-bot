c2c7c8d44e1d0cadcc77f90a30ec55c428f27bfd
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-09-15 13:28:39 -0700

    can request

--- dnd_bot.py
+++ dnd_bot.py
@@ -1,26 +1,35 @@
 import requests
     
 class Dnd_Bot:
-    
-    def __init__(self, fact=None):
+    # TODO: URL builder Randomizer
+    # TODO: Clean up output data
+    def __init__(self, url="", fact=None):
         self.fact = fact
+        self.url = url
         
-    def fact_request(self):
+    def ability_scores_url_builder(self):
+        """
+        Builds 
+        """
+        self.url =  "https://www.dnd5eapi.co/api/skills/arcana"
+        return self.url
+        
+    def fact_request(self, url=None):
         """
         Hits dnd5eapi and returns Dnd fact
         """
-        r = requests.get("https://www.dnd5eapi.co/api/ability-scores/str")
+        r = requests.get(self.url)
         self.fact = r.json()
-        print(self.fact)
         return
     
     def __str__(self):
         """
         Returns name and description
         """
-        return self.fact["full_name"], self.fact["desc"]
+        return self.fact["name"], self.fact["desc"]
 
 if __name__ == '__main__':
-    fact1 =Dnd_Bot()
+    fact1 = Dnd_Bot()
+    fact1.ability_scores_url_builder()
     fact1.fact_request()
     print(fact1.__str__())
