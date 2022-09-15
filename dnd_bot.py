fe689398081710ff148c441eea516011ec05d42f
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-09-15 14:58:44 -0700

    can import json data, test passing

--- dnd_bot.py
+++ dnd_bot.py
@@ -1,15 +1,22 @@
 import requests
+import json
     
 class Dnd_Bot:
     # TODO: URL builder Randomizer
     # TODO: Clean up output data
-    def __init__(self, url="", fact=None):
+    def __init__(self, data=None, url="", fact=None):
         self.fact = fact
         self.url = url
+        self.data = data
+        
+    
+    def import_url_from_json(self):
+        with open('url_builder_data.json') as f:
+            self.data = json.load(f)
+        
         
     def ability_scores_url_builder(self):
         """
-        Builds 
         """
         self.url =  "https://www.dnd5eapi.co/api/skills/arcana"
         return self.url
@@ -30,6 +37,8 @@ class Dnd_Bot:
 
 if __name__ == '__main__':
     fact1 = Dnd_Bot()
+    fact1.import_url_from_json()
+    print(fact1.data['results'][0]['url'])
     fact1.ability_scores_url_builder()
     fact1.fact_request()
-    print(fact1.__str__())
+    # print(fact1.__str__())
--- test/dnd_bot_test.py
+++ test/dnd_bot_test.py
@@ -1,4 +1,5 @@
 import pytest
+import json
 from dnd_bot import Dnd_Bot
 
 def test_exists():
@@ -23,7 +24,12 @@ def test_request_url_return(url_setup, url_request_return ):
     fact1.url = url_setup
     fact1.fact_request()
     assert fact1.fact == url_request_return
-
+    
+def test_json_data_url(url_part):
+    fact1 = Dnd_Bot()
+    fact1.import_url_from_json()
+    assert fact1.data['results'][0]['url'] == '/api/alignments/chaotic-evil'
+    
 @pytest.fixture
 def url_setup():
      url = "https://www.dnd5eapi.co/api/ability-scores/str"
@@ -33,3 +39,9 @@ def url_setup():
 def url_request_return():
     strength_return = {'index': 'str', 'name': 'STR', 'full_name': 'Strength', 'desc': ['Strength measures bodily power, athletic training, and the extent to which you can exert raw physical force.', 'A Strength check can model any attempt to lift, push, pull, or break something, to force your body through a space, or to otherwise apply brute force to a situation. The Athletics skill reflects aptitude in certain kinds of Strength checks.'], 'skills': [{'name': 'Athletics', 'index': 'athletics', 'url': '/api/skills/athletics'}], 'url': '/api/ability-scores/str'}
     return strength_return
+
+@pytest.fixture
+def url_part():
+    with open('url_builder_data.json') as f:
+        data = json.load(f)
+    return data
