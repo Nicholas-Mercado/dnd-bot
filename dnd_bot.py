0fa061ec06c234b54d7fd538f1b9a939c4f78e29
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-09-15 12:28:24 -0700

    added str

--- dnd_bot.py
+++ dnd_bot.py
@@ -3,15 +3,24 @@ import requests
 class Dnd_Bot:
     
     def __init__(self, fact=None):
-        self.fact=fact
+        self.fact = fact
         
     def fact_request(self):
+        """
+        Hits dnd5eapi and returns Dnd fact
+        """
         r = requests.get("https://www.dnd5eapi.co/api/ability-scores/str")
-        self.fact = r.json
-        
+        self.fact = r.json()
+        print(self.fact)
+        return
     
+    def __str__(self):
+        """
+        Returns name and description
+        """
+        return self.fact["full_name"], self.fact["desc"]
 
 if __name__ == '__main__':
     fact1 =Dnd_Bot()
     fact1.fact_request()
-    print(fact1)
+    print(fact1.__str__())
