4e4518ba86c0b03e1188d8b544b8b469375051b9
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-09-16 21:41:13 -0700

    small refactor (type hinting)

--- dnd_bot.py
+++ dnd_bot.py
@@ -5,18 +5,24 @@ import random
 class Dnd_Bot:
     # TODO: Clean up output data
     def __init__(self, url_endpoint=None, data=None, url="", fact=None):
-        self.fact = fact
-        self.url = url
-        self.data = data
-        self.url_endpoint = url_endpoint
+        self.fact: dict = fact
+        self.url: str = url
+        self.data: dict = data
+        self.url_endpoint: str = url_endpoint
         
     
-    def import_url_from_json(self):
+    def retrieve_url(self):
+        """
+        Reads json file containing url endpoints 
+        """
         with open('url_builder_data.json') as f:
             self.data = json.load(f)
         return
     
     def random_url_data(self):
+        """
+        Randomly chooses and returns url endpoint
+        """
         random_number = random.randint(1, 417)
         self.url_endpoint = self.data['results'][random_number]['url']
         return    
@@ -34,7 +40,7 @@ class Dnd_Bot:
         """
         Hits dnd5eapi and returns Dnd fact
         """
-        r = requests.get(self.url)
+        r: dict = requests.get(self.url)
         self.fact = r.json()
         return
     
@@ -46,7 +52,7 @@ class Dnd_Bot:
 
 if __name__ == '__main__':
     fact1 = Dnd_Bot()
-    fact1.import_url_from_json()
+    fact1.retrieve_url()
     fact1.random_url_data()
     fact1.url_builder()
     fact1.fact_request()
